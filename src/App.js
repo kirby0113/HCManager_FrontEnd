850e5ae8756df59ca07a3548b761e91011646044
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-23 10:09:32 +0900

    BodyをApp.jsに追加

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Body from "./components/Body";
+
 
 
 const App = () => {
     return (
         <div>
             <Header></Header>
-            <div>
-                Hello World!
-            </div>
+            <Body></Body>
         </div>
     );
 }
