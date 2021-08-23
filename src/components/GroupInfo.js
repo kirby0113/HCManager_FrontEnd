97f01002bf532e37b17aa24966faec848f0b8ce0
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-23 14:11:24 +0900

    GroupInfo.js内の表示項目の追加

--- src/components/GroupInfo.js
+++ src/components/GroupInfo.js
@@ -1,9 +1,25 @@
+import "./GroupInfo.css";
 
 const GroupInfo = (props) => {
 
     return (
-        <div>
-            {props.data.createdBy}
+        <div className="GroupInfoFrame">
+            <div>
+                <span className="elementName">グループ名</span>
+                {props.data.groupName}
+            </div>
+            <div>
+                <span className="elementName">作成者</span>
+                {props.data.createdBy}
+            </div>
+            <div>
+                <span className="elementName">アクセスキー</span>
+                {props.data.accesskey}
+            </div>
+            <div>
+                <span className="elementName">作成日</span>
+                {props.data.date}
+            </div>
         </div>
     )
 }
