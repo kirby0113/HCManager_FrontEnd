cdfe68b79e3e74490c7526dbb9fd02d085a88c1a
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-23 10:08:56 +0900

    Bodyコンポーネントの作成
    ダミーデータを作ってます

---
+++ src/components/Body.js
@@ -0,0 +1,45 @@
+import GroupInfo from "./GroupInfo"
+
+const dummyData = [
+    {
+        groupName:"group1",
+        createdBy:"admin1",
+        accesskey:"test",
+        date:"dummy1"
+    },
+    {
+        groupName:"group2",
+        createdBy:"admin2",
+        accesskey:"test",
+        date:"dummy2"
+    },
+    {
+        groupName:"group3",
+        createdBy:"admin3",
+        accesskey:"test",
+        date:"dummy3"
+    },
+    {
+        groupName:"group4",
+        createdBy:"admin4",
+        accesskey:"test",
+        date:"dummy4"
+    },
+    {
+        groupName:"group5",
+        createdBy:"admin5",
+        accesskey:"test",
+        date:"dummy5"
+    },
+]
+
+const Body = () => {
+
+    return (
+        <div>
+            {dummyData.map((data) => <GroupInfo data={data}></GroupInfo>)}
+        </div>
+    );
+}
+
+export default Body;
