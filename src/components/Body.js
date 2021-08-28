3c39ecb33f045c0ac5f73d560b7863b59b36f2c6
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-28 15:04:49 +0900

    ページネーションの作成

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,9 @@
+import React,{useState} from "react";
+import ReactPaginate from 'react-paginate';
+
 import GroupInfo from "./GroupInfo"
 import "./Body.css"
+
 
 const dummyData = [
     {
@@ -32,13 +36,45 @@ const dummyData = [
         accesskey:"test",
         date:"dummy5"
     },
-]
+];
+
 
 const Body = () => {
 
+    const handlePageChange = (data) => {
+        let page_number = data['selected']; // クリックした部分のページ数が{selected: 2}のような形で返ってくる
+        setOffset(page_number*perPage); // offsetを変更し、表示開始するアイテムの番号を変更
+    }
+
+    const [offset,setOffset] = useState(0);
+    const perPage = 1; // 1ページあたりに表示したいアイテムの数
+
     return (
         <div className="Body">
-            {dummyData.map((data) => <GroupInfo data={data}></GroupInfo>)}
+            <ReactPaginate
+            style={{ marginTop: 10, marginBottom: 100 }}
+            pageCount={Math.ceil(dummyData.length/perPage)} // 全部のページ数。端数の場合も考えて切り上げに。
+            marginPagesDisplayed={5}
+            pageRangeDisplayed={10}
+            onPageChange={handlePageChange}
+            containerClassName='pagination'
+            pageClassName='page-item'
+            pageLinkClassName='page-link'
+            activeClassName='active'
+            previousLabel='<'
+            nextLabel='>'
+            previousClassName='page-item'
+            nextClassName='page-item'
+            previousLinkClassName='page-link'
+            nextLinkClassName='page-link'
+            disabledClassName='disabled'
+            breakLabel='...'
+            breakClassName='page-item'
+            breakLinkClassName='page-link'
+            />
+            <div className="GroupList">
+            {dummyData.slice(offset,offset + perPage).map((data) => <GroupInfo data={data}></GroupInfo>)}
+            </div>
         </div>
     );
 }
