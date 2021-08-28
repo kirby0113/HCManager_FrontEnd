ce4301f36921b0c39c174c226661f2eb7f6f6d67
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-28 15:04:14 +0900

    bootstrapの適用

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const App = () => {
