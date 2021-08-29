2f4988888da4a1623f37bd52dc89441ba08540c2
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-29 21:57:34 +0900

    Headerにホームリンクを設定

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ import "./Header.css";
 const Header = () => {
     return (
         <div className="Header">
+            <a className="HeaderLink" href="/">
             HelloCManager
+            </a>
         </div>
     )
 }
