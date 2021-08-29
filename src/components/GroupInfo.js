f3c42e034847e253f8b845d70043b64ba7035c02
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-08-29 12:01:52 +0900

    GroupInfoに削除ボタンと詳細確認ボタンを追加

--- src/components/GroupInfo.js
+++ src/components/GroupInfo.js
@@ -1,26 +1,37 @@
 import "./GroupInfo.css";
 
+
 const GroupInfo = (props) => {
 
     return (
-        <div className="GroupInfoFrame">
-            <div>
-                <span className="elementName">グループ名</span>
-                {props.data.groupName}
+            <div className="GroupInfoFrame">
+                <div className="GroupInfoGrid">
+                    <div>
+                        <span className="elementName">グループ名</span>
+                        {props.data.groupName}
+                    </div>
+                    <div>
+                        <span className="elementName">作成者</span>
+                        {props.data.createdBy}
+                    </div>
+                    <div>
+                        <span className="elementName">アクセスキー</span>
+                        {props.data.accesskey}
+                    </div>
+                    <div>
+                        <span className="elementName">作成日</span>
+                        {props.data.date}
+                    </div>
+                </div>
+                <div className="GroupInfoLinkGrid">
+                    <div class="GroupInfoDeleteButtonFrame">
+                    <span className="GroupInfoDeleteButton">削除する</span>
+                    </div>
+                    <div className="GroupInfoDetailButtonFrame">
+                    <span className="GroupInfoDetailButton">詳細を見る</span>
+                    </div>
+                </div>
             </div>
-            <div>
-                <span className="elementName">作成者</span>
-                {props.data.createdBy}
-            </div>
-            <div>
-                <span className="elementName">アクセスキー</span>
-                {props.data.accesskey}
-            </div>
-            <div>
-                <span className="elementName">作成日</span>
-                {props.data.date}
-            </div>
-        </div>
     )
 }
 
