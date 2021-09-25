fce78c86d7d2213061e3c8e2846254e9e704cb5d
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-09-25 16:25:09 +0900

    NavigationをMaterialUI仕様に変更

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
+import {useState} from "react";
+
 import "./Header.css";
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import {IconButton} from "material-ui";
+import Navigation from "./Navigation";
 const Header = () => {
+    const [openNavigation,setOpenNavigation] = useState(false);
+
     return (
-        <div className="Header">
-            <a className="HeaderLink" href="/">
-            HelloCManager
-            </a>
+        <div>
+        <Navigation open={openNavigation} setOpenNavigation={setOpenNavigation}></Navigation>
+        <AppBar position="static" className="Header" onLeftIconButtonTouchTap={() => setOpenNavigation(true)}>
+        <IconButton 
+                color="inherit"
+                aria-label="Menu"
+                onClick={() => setOpenNavigation(true)}>
+		        <MenuIcon className="MenuIcon"></MenuIcon>
+            </IconButton>
+            <Typography className="Title" variant="h3" color="inherit">
+                    HelloC Manager
+                </Typography>
+        </AppBar>
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,12 +5,15 @@ import {
   Link,
 } from "react-router-dom";
 
+import { MenuItem, Drawer } from 'material-ui';
+
+
 
 import "./Navigation.css";
 
 const StyledLink = styled(Link)`
 text-decoration:none;
-color:#fff;
+color:#000;
 font-size:16px;
 letter-spacing:1.4px;
 "&:hover":{
@@ -18,37 +21,75 @@ letter-spacing:1.4px;
 }
 `
 
-const Navigation = () => {
+const Navigation = (props) => {
     return (
-          <nav className="Navigation">
-            <ul className="NavigationList">
-            <StyledLink to="/">
-                <li className="NavigationUnit">
-                    <span >TOP</span>
-                </li>
-              </StyledLink>
-              <StyledLink to="/group">
-                <li className="NavigationUnit">
-                    <span >グループ一覧</span>
-                </li>
-              </StyledLink>
-              <StyledLink to="/users">
-                <li className="NavigationUnit">
-                    <span >ユーザ一覧</span>
-                </li>
-              </StyledLink>
-              <StyledLink to="/questions">
-                <li className="NavigationUnit">
-                    <span >問題一覧</span>
-                </li>
-              </StyledLink>
-              <StyledLink to="/teachingMaterials">
-                <li className="NavigationUnit">
-                    <span >教材一覧</span>
-                </li>
-              </StyledLink>
-            </ul>
-        </nav>
+        //   <nav className="Navigation">
+        //     <ul className="NavigationList">
+        //     <StyledLink to="/">
+        //         <li className="NavigationUnit">
+        //             <span >TOP</span>
+        //         </li>
+        //       </StyledLink>
+        //       <StyledLink to="/group">
+        //         <li className="NavigationUnit">
+        //             <span >グループ一覧</span>
+        //         </li>
+        //       </StyledLink>
+        //       <StyledLink to="/users">
+        //         <li className="NavigationUnit">
+        //             <span >ユーザ一覧</span>
+        //         </li>
+        //       </StyledLink>
+        //       <StyledLink to="/questions">
+        //         <li className="NavigationUnit">
+        //             <span >問題一覧</span>
+        //         </li>
+        //       </StyledLink>
+        //       <StyledLink to="/teachingMaterials">
+        //         <li className="NavigationUnit">
+        //             <span >教材一覧</span>
+        //         </li>
+        //       </StyledLink>
+        //     </ul>
+        // </nav>
+        <Drawer    
+        docked={false}
+        width={200}
+        open={props.open}
+        onRequestChange={() => props.setOpenNavigation(false)}
+        >
+            <StyledLink to="/" onClick={() => props.setOpenNavigation(false)}>
+              <MenuItem>
+                TOP
+              </MenuItem>
+            </StyledLink>
+
+            <StyledLink to="/group" onClick={() => props.setOpenNavigation(false)}>
+              <MenuItem>
+              グループ一覧
+              </MenuItem>
+            </StyledLink>
+
+            <StyledLink to="/users" onClick={() => props.setOpenNavigation(false)}>
+              <MenuItem>
+              ユーザー覧
+              </MenuItem>
+            </StyledLink>
+
+            <StyledLink to="/questions" onClick={() => props.setOpenNavigation(false)}>
+              <MenuItem>
+              問題一覧
+              </MenuItem>
+            </StyledLink>
+
+            <StyledLink to="/teachingMaterials" onClick={() => props.setOpenNavigation(false)}>
+              <MenuItem>
+              教材一覧
+              </MenuItem>
+            </StyledLink>
+
+        </Drawer>
+
     )
 }
 
