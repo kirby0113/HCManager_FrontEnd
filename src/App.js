5cdeabbc8f61fd759d4ec5fcc0e965fe68dd1376
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-11-14 07:28:53 +0900

    API接続確認

--- src/App.js
+++ src/App.js
@@ -1,85 +1,83 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React, {useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-
-
 //一覧ページ等
-import Home from "./components/Home/Home";
-import Header from "./components/Header";
-import GroupBody from "./components/Group/Body";
-import UsersBody from "./components/Users/Body";
-import QuestionsBody from "./components/Questions/Questions";
-import TeachingMaterialsBody from "./components/TeachingMaterials/TeachingMaterials";
+import Home from './components/Home/Home';
+import Header from './components/Header';
+import GroupBody from './components/Group/Body';
+import UsersBody from './components/Users/Body';
+import QuestionsBody from './components/Questions/Questions';
+import TeachingMaterialsBody from './components/TeachingMaterials/TeachingMaterials';
 
 //詳細ページ
-import GroupDetail from "./components/GroupDetail/GroupDetail";
-import TeachingMaterialDetail from "./components/TMDetail/TeachingMaterialDetail";
+import GroupDetail from './components/GroupDetail/GroupDetail';
+import TeachingMaterialDetail from './components/TMDetail/TeachingMaterialDetail';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const App = () => {
-    return (
-      <Router>
-        <MuiThemeProvider>
-         <div>
+  useEffect(() => {
+    fetch('http://192.168.100.130:4000/api/books/') //api
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }, []);
 
+  return (
+    <Router>
+      <MuiThemeProvider>
+        <div>
           <Header></Header>
           <Switch>
-
             {/* 一覧ページのルーティング */}
-            <Route exact path="/group">
+            <Route exact path='/group'>
               <Group></Group>
             </Route>
-            <Route exact path="/users">
+            <Route exact path='/users'>
               <Users />
             </Route>
-            <Route exact path="/questions">
+            <Route exact path='/questions'>
               <Questions />
             </Route>
-            <Route exact path="/teachingMaterials">
+            <Route exact path='/teachingMaterials'>
               <TeachingMaterials />
             </Route>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home />
             </Route>
 
             {/* 詳細ページのルーティング */}
 
-            <Route render = {() => <GroupDetail/>
-            } path="/group/detail/:id(\d+)"/>
+            <Route render={() => <GroupDetail />} path='/group/detail/:id(\d+)' />
 
-            <Route render = {() => <TeachingMaterialDetail/>
-            } path="/TeachingMaterial/detail/:id(\d+)"/>
-
+            <Route render={() => <TeachingMaterialDetail />} path='/TeachingMaterial/detail/:id(\d+)' />
           </Switch>
         </div>
-        </MuiThemeProvider>
-      </Router>
-    );
+      </MuiThemeProvider>
+    </Router>
+  );
+};
+
+function Group() {
+  return (
+    <div>
+      <GroupBody></GroupBody>
+    </div>
+  );
 }
 
-  function Group() {
-    return <div>
-            <GroupBody></GroupBody>
-    </div>;
-  }
-  
-  function Users() {
-    return <UsersBody></UsersBody>;
-  }
+function Users() {
+  return <UsersBody></UsersBody>;
+}
 
-  function Questions() {
-    return <QuestionsBody></QuestionsBody>
-  }
-  function TeachingMaterials(){
-    return <TeachingMaterialsBody></TeachingMaterialsBody>
-  }
+function Questions() {
+  return <QuestionsBody></QuestionsBody>;
+}
+function TeachingMaterials() {
+  return <TeachingMaterialsBody></TeachingMaterialsBody>;
+}
 
 export default App;
