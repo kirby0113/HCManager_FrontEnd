6e819a8e1430676ea653cd4d0c240ed31ccf1e9a
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-11-14 16:44:14 +0900

    TM一覧のAPI表示を実装

--- src/components/TeachingMaterials/TeachingMaterialInfo.js
+++ src/components/TeachingMaterials/TeachingMaterialInfo.js
@@ -1,62 +1,74 @@
-import "./TeachingMaterialInfo.css";
+import './TeachingMaterialInfo.css';
 
-import Card from "@material-ui/core/Card";
+import {useEffect, useState} from 'react';
+import {UsersAPI} from '../../APILink';
+
+import Card from '@material-ui/core/Card';
 
 import styled from 'styled-components';
 
-import {
-  Link,
-} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 const StyledLink = styled(Link)`
-text-decoration:none;
-color:#000;
-font-size:16px;
-letter-spacing:1.4px;
-"&:hover":{
-  text-decoration:none;
-}
-`
+  text-decoration: none;
+  color: #000;
+  font-size: 16px;
+  letter-spacing: 1.4px;
+  '&:hover': {
+    text-decoration: none;
+  }
+`;
 
-
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 
 /* TM = TeachingMaterial */
 
 const GroupInfo = (props) => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    fetch(UsersAPI + '/' + props.data.user_id) //api
+      .then((res) => res.json())
+      .then((json) => {
+        setUser(json);
+      });
+  }, []);
 
-    return (
-            <Card className="TMInfoFrame">
-                <div className="TMInfoGrid">
-                    <div>
-                        <span className="elementName">教材名</span>
-                        {props.data.TeachingMaterialName}
-                    </div>
-                    <div>
-                        <span className="elementName">作成者</span>
-                        {props.data.createdBy}
-                    </div>
-                    <div>
-                        <span className="elementName">アクセスキー</span>
-                        {props.data.accesskey}
-                    </div>
-                    <div>
-                        <span className="elementName">作成日</span>
-                        {props.data.date}
-                    </div>
-                </div>
-                <div className="TMInfoLinkGrid">
-                    <div className="TMInfoDeleteButtonFrame">
-                    <Button variant="contained" color="secondary" className="TMInfoDeleteButton">削除する</Button>
-                    </div>
-                    <div className="TMInfoDetailButtonFrame">
-                    <StyledLink to={"/TeachingMaterial/detail/".concat(props.data.id)}>
-                    <Button variant="contained" color="primary" className="TMInfoDetailButton">詳細を見る</Button>
-                    </StyledLink>
-                    </div>
-                </div>
-            </Card>
-    )
-}
+  return (
+    <Card className='TMInfoFrame'>
+      <div className='TMInfoGrid'>
+        <div>
+          <span className='elementName'>教材名</span>
+          {props.data.name}
+        </div>
+        <div>
+          <span className='elementName'>作成者</span>
+          {user ? user.name : ''}
+        </div>
+        <div>
+          <span className='elementName'>アクセスキー</span>
+          {props.data.access_key}
+        </div>
+        <div>
+          <span className='elementName'>作成日</span>
+          {props.data.created_at}
+        </div>
+      </div>
+      <div className='TMInfoLinkGrid'>
+        <div className='TMInfoDeleteButtonFrame'>
+          <Button variant='contained' color='secondary' className='TMInfoDeleteButton'>
+            削除する
+          </Button>
+        </div>
+        <div className='TMInfoDetailButtonFrame'>
+          <StyledLink to={'/TeachingMaterial/detail/'.concat(props.data.book_id)}>
+            <Button variant='contained' color='primary' className='TMInfoDetailButton'>
+              詳細を見る
+            </Button>
+          </StyledLink>
+        </div>
+      </div>
+    </Card>
+  );
+};
 
 export default GroupInfo;
