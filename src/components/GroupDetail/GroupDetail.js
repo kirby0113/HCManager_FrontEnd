40884a7a18b19b008abcb4ad9d53fca38067633d
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-11-14 13:43:42 +0900

    グループ詳細ページにAPI処理を実装
    現在book_idが定義されてないため、教材が取得できない状態です。

--- src/components/GroupDetail/GroupDetail.js
+++ src/components/GroupDetail/GroupDetail.js
@@ -1,156 +1,166 @@
-import React from "react";
+import {useState, useEffect} from 'react';
 
-import { useParams } from "react-router";
+import {useParams} from 'react-router';
 
-import TeachingMaterialInfo from "../TeachingMaterials/TeachingMaterialInfo";
+import {GroupsAPI, UsersAPI, BooksAPI} from '../../APILink';
 
-import Button from "@material-ui/core/Button";
+import TeachingMaterialInfo from '../TeachingMaterials/TeachingMaterialInfo';
 
-import "./GroupDetail.css";
+import Button from '@material-ui/core/Button';
 
+import './GroupDetail.css';
 
 //とりあえずダミーデータにしてます（本来はAPIから取得）
-const dummyTM = [
-    {
-        TeachingMaterialName:"TM1",
-        createdBy:"admin1",
-        accesskey:"test",
-        date:"dummy1",
-        groupId:1
-    },
-    {
-        TeachingMaterialName:"TM2",
-        createdBy:"admin2",
-        accesskey:"test",
-        date:"dummy2",
-        groupId:2
-    },
-    {
-        TeachingMaterialName:"TM3",
-        createdBy:"admin3",
-        accesskey:"test",
-        date:"dummy3",
-        groupId:3
-    },
-    {
-        TeachingMaterialName:"TM4",
-        createdBy:"admin4",
-        accesskey:"test",
-        date:"dummy4",
-        groupId:4
-    },
-    {
-        TeachingMaterialName:"TM5",
-        createdBy:"admin5",
-        accesskey:"test",
-        date:"dummy5",
-        groupId:5
-    },
-];
-
-const dummyGroup = [
-    {
-        id:1,
-        groupName:"group1",
-        createdBy:"admin1",
-        accesskey:"test",
-        date:"dummy1",
-        overview:"これはtestグループです。\n改行テスト"
-    },
-    {
-        id:2,
-        groupName:"group2",
-        createdBy:"admin2",
-        accesskey:"test",
-        date:"dummy2",
-        overview:"これはtestグループです。"
-    },
-    {
-        id:3,
-        groupName:"group3",
-        createdBy:"admin3",
-        accesskey:"test",
-        date:"dummy3",
-        overview:"これはtestグループです。"
-    },
-    {
-        id:4,
-        groupName:"group4",
-        createdBy:"admin4",
-        accesskey:"test",
-        date:"dummy4",
-        overview:"これはtestグループです。"
-    },
-    {
-        id:5,
-        groupName:"group5",
-        createdBy:"admin5",
-        accesskey:"test",
-        date:"dummy5",
-        overview:"これはtestグループです。"
-    },
-];
-
+// const dummyTM = [
+//   {
+//     TeachingMaterialName: 'TM1',
+//     createdBy: 'admin1',
+//     accesskey: 'test',
+//     date: 'dummy1',
+//     groupId: 1,
+//   },
+//   {
+//     TeachingMaterialName: 'TM2',
+//     createdBy: 'admin2',
+//     accesskey: 'test',
+//     date: 'dummy2',
+//     groupId: 2,
+//   },
+//   {
+//     TeachingMaterialName: 'TM3',
+//     createdBy: 'admin3',
+//     accesskey: 'test',
+//     date: 'dummy3',
+//     groupId: 3,
+//   },
+//   {
+//     TeachingMaterialName: 'TM4',
+//     createdBy: 'admin4',
+//     accesskey: 'test',
+//     date: 'dummy4',
+//     groupId: 4,
+//   },
+//   {
+//     TeachingMaterialName: 'TM5',
+//     createdBy: 'admin5',
+//     accesskey: 'test',
+//     date: 'dummy5',
+//     groupId: 5,
+//   },
+// ];
 
 const GroupDetail = () => {
-    const param = useParams();
-    const detail = dummyGroup.find(element => element.id == param["id"]);
+  const param = useParams();
 
-    return (
-        <div>
-            <div className="GroupDetailPageTitleFrame">
-                <span className="GroupDetailPageTitle">
-                    グループ詳細
-                </span>
-            </div>
+  const [GroupData, setGroupData] = useState();
+  const [CreatedBy, setCreatedBy] = useState();
+  const [Books, setBooks] = useState();
 
-            <div className="editGroupDetailButtonFrame">
-                <Button variant="contained" color="secondary" className="editGroupDetailButton">編集</Button>
-            </div>
-            <div class="GroupDetailFrame">
-                <div className="GroupDetailTopGrid">
-                    <div>
-                        <span className="elementName">グループ名</span>
-                        {detail.groupName}
-                    </div>
-                    <div>
-                        <span className="elementName">作成者</span>
-                        {detail.createdBy}
-                    </div>
-                    <div>
-                        <span className="elementName">アクセスキー</span>
-                        {detail.accesskey}
-                    </div>
-                    <div>
-                        <span className="elementName">作成日</span>
-                        {detail.date}
-                    </div>
-                </div>
-                <div className="GroupDetailBottom">
-                    <div>
-                    <span className="elementName">グループ概略</span>
-                    </div>
-                    <div class="GroupDetailTextRange">{detail.overview.split(/(\n)/).map((item) => {
-                        return item.match(/\n/) ? <br /> : item
-                    })}</div>
-                </div>
-            </div>
+  useEffect(() => {
+    //最初にGroupデータを取得
+    fetch(GroupsAPI + '/' + param['id']) //api
+      .then((res) => res.json())
+      .then((json) => {
+        setGroupData(json);
+        console.log(json);
+      });
+  }, []);
 
-            <div className="GroupDetailPageTitleFrame-sub">
-                <span className="GroupDetailPageTitle-sub">
-                    グループ内教材一覧
-                </span>
-            </div>
+  useEffect(() => {
+    //Groupデータ更新時に作成者名を取得
+    if (typeof GroupData !== 'undefined') {
+      fetch(UsersAPI + '/' + param['id']) //api
+        .then((res) => res.json())
+        .then((json) => {
+          setCreatedBy(json.name);
+        });
+    }
+  }, [GroupData]);
 
-            <div className="addTMButtonFrame">
-                <Button variant="contained" color="primary" className="addTMButton" >追加</Button>
-                <Button variant="contained" color="primary" className="addTMsButton" >複数追加</Button>
+  useEffect(() => {
+    //Groupデータ更新時に教材を取得
+    if (typeof GroupData !== 'undefined') {
+      fetch(BooksAPI + '/' + GroupData.book_id) //api *book_idが定義されてないので取得できない状態
+        .then((res) => res.json())
+        .then((json) => {
+          setBooks(json);
+        });
+    }
+  }, [GroupData]);
+
+  return (
+    <div>
+      <div className='GroupDetailPageTitleFrame'>
+        <span className='GroupDetailPageTitle'>グループ詳細</span>
+      </div>
+
+      <div className='editGroupDetailButtonFrame'>
+        <Button variant='contained' color='secondary' className='editGroupDetailButton'>
+          編集
+        </Button>
+      </div>
+      <div class='GroupDetailFrame'>
+        {GroupData ? (
+          <div className='GroupDetailTopGrid'>
+            <div>
+              <span className='elementName'>グループ名</span>
+              {GroupData.name}
             </div>
-            <div className="TMList">
-                    {dummyTM.filter(element => element.groupId == param["id"]).map((data) => <TeachingMaterialInfo data={data} key={data.groupName}></TeachingMaterialInfo>)}
+            <div>
+              <span className='elementName'>作成者</span>
+              {CreatedBy ? CreatedBy : ''}
             </div>
+            <div>
+              <span className='elementName'>アクセスキー</span>
+              {GroupData.access_key}
+            </div>
+            <div>
+              <span className='elementName'>作成日</span>
+              {GroupData.created_at}
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+        <div className='GroupDetailBottom'>
+          <div>
+            <span className='elementName'>グループ概略</span>
+          </div>
+          {GroupData ? (
+            <div class='GroupDetailTextRange'>
+              {GroupData.summary.split(/(\n)/).map((item) => {
+                return item.match(/\n/) ? <br /> : item;
+              })}
+            </div>
+          ) : (
+            ''
+          )}
         </div>
-    );
-}
+      </div>
+
+      <div className='GroupDetailPageTitleFrame-sub'>
+        <span className='GroupDetailPageTitle-sub'>グループ内教材一覧</span>
+      </div>
+
+      <div className='addTMButtonFrame'>
+        <Button variant='contained' color='primary' className='addTMButton'>
+          追加
+        </Button>
+        <Button variant='contained' color='primary' className='addTMsButton'>
+          複数追加
+        </Button>
+      </div>
+      {Books ? (
+        <div className='TMList'>
+          {Books.map((data) => (
+            <TeachingMaterialInfo data={data} key={data.groupName}></TeachingMaterialInfo>
+          ))}
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
 
 export default GroupDetail;
