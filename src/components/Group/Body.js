3b7aec569672f1aa87b1f5cf534d6b3ec0374ac3
kirby0113 <2000ryo0113@ezweb.ne.jp>
2021-11-22 19:39:04 +0900

    グループ作成用モーダルの作成

--- src/components/Group/Body.js
+++ src/components/Group/Body.js
@@ -1,22 +1,15 @@
 import {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
 
 import Button from '@material-ui/core/Button';
 
 import GroupInfo from './GroupInfo';
 import Pagination from './Pagination';
-import CreateGroupModal from './CreateGroupModal';
 
 import {GroupsAPI} from '../../APILink';
 
-import './Body.css';
+import {CreateGroupModal} from './CreateGroupModal';
 
-const CreateGroup = (props) => {
-  return ReactDOM.createPortal(
-    <CreateGroupModal modalVisible={props.modalVisible} setModalVisible={props.setModalVisible}></CreateGroupModal>,
-    document.getElementById('modal-creategroup')
-  );
-};
+import './Body.css';
 
 const Body = () => {
   const [offset, setOffset] = useState(0);
@@ -32,38 +25,38 @@ const Body = () => {
       });
   }, []);
 
-  Groups ? console.log(Groups) : '';
-
   return (
-    <div className='Body'>
-      <CreateGroup modalVisible={modalVisible} setModalVisible={setModalVisible}></CreateGroup>
-      <div className='PageTitleFrame'>
-        <span className='PageTitle'>グループ一覧</span>
-      </div>
-      {Groups ? <Pagination setOffset={setOffset} dataleng={Groups.length} perPage={perPage}></Pagination> : ''}
-      <div className='addGroupButtonFrame'>
-        <Button
-          variant='contained'
-          color='primary'
-          className='addGroupButton'
-          onClick={() => {
-            setModalVisible(true);
-          }}
-        >
-          グループ追加
-        </Button>
-      </div>
-
-      {Groups ? (
-        <div className='GroupList'>
-          {Groups.slice(offset, offset + perPage).map((data) => (
-            <GroupInfo data={data} key={data.group_id}></GroupInfo>
-          ))}
+    <div>
+      <div className='Body'>
+        <div className='PageTitleFrame'>
+          <span className='PageTitle'>グループ一覧</span>
         </div>
-      ) : (
-        ''
-      )}
-      {Groups ? <Pagination setOffset={setOffset} dataleng={Groups.length} perPage={perPage}></Pagination> : ''}
+        {Groups ? <Pagination setOffset={setOffset} dataleng={Groups.length} perPage={perPage}></Pagination> : ''}
+        <div className='addGroupButtonFrame'>
+          <Button
+            variant='contained'
+            color='primary'
+            className='addGroupButton'
+            onClick={() => {
+              setModalVisible(true);
+            }}
+          >
+            グループ追加
+          </Button>
+        </div>
+
+        {Groups ? (
+          <div className='GroupList'>
+            {Groups.slice(offset, offset + perPage).map((data) => (
+              <GroupInfo data={data} key={data.group_id}></GroupInfo>
+            ))}
+          </div>
+        ) : (
+          ''
+        )}
+        {Groups ? <Pagination setOffset={setOffset} dataleng={Groups.length} perPage={perPage}></Pagination> : ''}
+      </div>
+      {modalVisible ? <CreateGroupModal onClose={() => setModalVisible(false)}></CreateGroupModal> : ''}
     </div>
   );
 };
--- src/components/Group/CreateGroupModal.js
+++ src/components/Group/CreateGroupModal.js
@@ -1,18 +1,121 @@
-import "./CreateGroupModal.css";
+import {useState, useEffect} from 'react';
+import styled from 'styled-components';
 
-const CreateGroupModal = (props) => {
-    const overlayClass = props.modalVisible ? "Modal-Overlay" : "Modal-Overlay hiddenOverlay";
-    const FormClass = props.modalVisible ? "CreateGroupForm" : "CreateGroupForm hiddenForm";
-    return <div>
-        <div className={overlayClass} onClick={() => {props.setModalVisible(false)}}>
-            &nbsp;
-        </div>
-        <form>
-            <div className={FormClass}>
-                Form建設予定地
-            </div>
-        </form>
+import Button from '@material-ui/core/Button';
+
+import {UsersAPI} from '../../APILink';
+
+import {Modal} from '../Modal';
+import {Overray} from '../Overray';
+
+const InputUnit = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  grid-column-gap: 30px;
+  padding-bottom: 30px;
+`;
+
+const CreateGroupButton = styled(Button)`
+  margin-right: 20px !important;
+  font-size: 18px !important;
+  border: solid 2px #777;
+  background-color: #ddd;
+  padding: 5px 20px !important;
+  box-shadow: 5px 5px 5px #00000040;
+  cursor: pointer;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const ModalTitle = styled.div`
+  text-align: left;
+  font-size: 20px;
+  padding: 7px 20px;
+  padding-left: 15px;
+  margin-bottom: 40px;
+  background: #f4f4f4; /*背景色*/
+  border-left: solid 8px #ff47ac; /*左線*/
+  border-bottom: solid 3px #d7d7d7; /*下線*/
+`;
+
+export const CreateGroupModal = (props) => {
+  const [Users, setUsers] = useState([]);
+  const [CreateGroupPostData, setCreateGroupPostData] = useState({
+    name: '',
+    summary: '',
+    access_key: '',
+    user_id: '',
+  });
+
+  useEffect(() => {
+    fetch(UsersAPI) //api
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json);
+      });
+  }, []);
+
+  const CreateGroupCheck = () => {
+    if (confirm('編集を保存しますか？')) {
+      CreateGroupFetch();
+      props.onClose();
+      // console.log(EditGroupPostData);
+    }
+  };
+
+  const CreateGroupFetch = () => {};
+
+  return (
+    <div>
+      <Modal>
+        <ModalTitle className='ModalTitle'>編集画面</ModalTitle>
+        <InputUnit>
+          <label htmlFor='groupname'>グループ名</label>
+          <input
+            type='text'
+            id='groupname'
+            value={CreateGroupPostData.name}
+            onChange={(e) => setCreateGroupPostData({...CreateGroupPostData, name: e.target.value})}
+          ></input>
+        </InputUnit>
+        <InputUnit>
+          <label htmlFor='username'>作成者</label>
+          <select
+            id='username'
+            value={CreateGroupPostData.user_id}
+            onChange={(e) => setCreateGroupPostData({...CreateGroupPostData, user_id: e.target.value})}
+          >
+            {Users.map((data) => (
+              <option value={data.user_id} key={data.user_id}>
+                {data.name}
+              </option>
+            ))}
+          </select>
+        </InputUnit>
+        <InputUnit>
+          <label htmlFor='accesskey'>アクセスキー</label>
+          <input
+            type='text'
+            id='accesskey'
+            value={CreateGroupPostData.access_key}
+            onChange={(e) => setCreateGroupPostData({...CreateGroupPostData, access_key: e.target.value})}
+          ></input>
+        </InputUnit>
+        <InputUnit>
+          <label htmlFor='summary'>グループ概略</label>
+          <textarea
+            id='summary'
+            value={CreateGroupPostData.summary}
+            rows='5'
+            onChange={(e) => setCreateGroupPostData({...CreateGroupPostData, summary: e.target.value})}
+          ></textarea>
+        </InputUnit>
+        <CreateGroupButton variant='contained' color='primary' onClick={() => CreateGroupCheck()}>
+          作成
+        </CreateGroupButton>
+      </Modal>
+      <Overray onClick={props.onClose}></Overray>
     </div>
-}
-
-export default CreateGroupModal;
+  );
+};
