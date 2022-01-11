852e8a40c99320e9ca9eab09bae310e5bb72b348
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-11 12:45:19 +0900

    refactor: Navigationコンポーネントのスタイルコードをリファクタ

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,49 +4,18 @@ import {Link} from 'react-router-dom';
 
 import {MenuItem, Drawer} from 'material-ui';
 
-import './Navigation.css';
-
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: #000;
   font-size: 16px;
   letter-spacing: 1.4px;
-  '&:hover': {
+  &:hover {
     text-decoration: none;
   }
 `;
 
 const Navigation = (props) => {
   return (
-    //   <nav className="Navigation">
-    //     <ul className="NavigationList">
-    //     <StyledLink to="/">
-    //         <li className="NavigationUnit">
-    //             <span >TOP</span>
-    //         </li>
-    //       </StyledLink>
-    //       <StyledLink to="/group">
-    //         <li className="NavigationUnit">
-    //             <span >グループ一覧</span>
-    //         </li>
-    //       </StyledLink>
-    //       <StyledLink to="/users">
-    //         <li className="NavigationUnit">
-    //             <span >ユーザ一覧</span>
-    //         </li>
-    //       </StyledLink>
-    //       <StyledLink to="/questions">
-    //         <li className="NavigationUnit">
-    //             <span >問題一覧</span>
-    //         </li>
-    //       </StyledLink>
-    //       <StyledLink to="/teachingMaterials">
-    //         <li className="NavigationUnit">
-    //             <span >教材一覧</span>
-    //         </li>
-    //       </StyledLink>
-    //     </ul>
-    // </nav>
     <Drawer docked={false} width={200} open={props.open} onRequestChange={() => props.setOpenNavigation(false)}>
       <StyledLink to='/' onClick={() => props.setOpenNavigation(false)}>
         <MenuItem>TOP</MenuItem>
