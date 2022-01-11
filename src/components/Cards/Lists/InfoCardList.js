731e6174f504a68c16ace264ee19b1c0ccd73bd8
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-11 20:17:16 +0900

    コンポーネント名修正

--- src/components/Cards/Lists/InfoCardList.js
+++ src/components/Cards/Lists/InfoCardList.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-const StyledCardList = styled.div`
+const StyledInfoCardList = styled.div`
   display: grid;
   grid-template-columns: 90vw;
   justify-content: center;
   grid-row-gap: 20px;
   margin: 30px 0;
 `;
-export const CardList = (props) => {
-  return <StyledCardList>{props.children}</StyledCardList>;
+export const InfoCardList = (props) => {
+  return <StyledInfoCardList>{props.children}</StyledInfoCardList>;
 };
