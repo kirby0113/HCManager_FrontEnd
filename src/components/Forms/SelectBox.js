38a2a6a5be184386208530271cd6c058c0eee744
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-15 21:35:11 +0900

    feat: Form用のコンポーネントを作成

---
+++ src/components/Forms/SelectBox.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+const StyledSelectBox = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  grid-column-gap: 30px;
+  padding-bottom: 30px;
+`;
+
+const Label = styled.label``;
+
+const Select = styled.select``;
+
+export const SelectBox = (props) => {
+  return (
+    <StyledSelectBox>
+      <Label htmlFor={props.id}>{props.label}</Label>
+      <Select id={props.id} onChange={props.onChange}>
+        <option value='' key=''>
+          -
+        </option>
+        {props.options.map((data) => (
+          <option value={data.user_id} key={data.user_id}>
+            {data.name}
+          </option>
+        ))}
+      </Select>
+    </StyledSelectBox>
+  );
+};
