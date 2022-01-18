54afe5dc02ab68e0d19205e96cbad0a085f614f0
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-19 00:23:32 +0900

    feat: Question詳細の内容を追加

--- src/pages/Question/QuestionDetail.js
+++ src/pages/Question/QuestionDetail.js
@@ -1,5 +1,54 @@
+import {useEffect, useState} from 'react';
+import parse from 'html-react-parser';
+import {getQuestion} from '../../components/API/QuestionAPIs';
+import {useParams} from 'react-router';
+
+import {PageTitle} from '../../components/Utilities/Title';
+import {Label} from '../../components/Utilities/Card/Label';
+import {DetailCard, DetailCardContent} from '../../components/Cards/DetailCard';
+
 const QuestionDetail = () => {
-  return <div>test</div>;
+  const param = useParams();
+  const [questionData, setQuestionData] = useState();
+  useEffect(() => {
+    getQuestion(param['id'])
+      .then((json) => {
+        setQuestionData(json);
+      })
+      .then(() => {
+        console.log(questionData);
+      });
+  }, []);
+  return (
+    <div>
+      <PageTitle color='blue'>問題詳細</PageTitle>
+      <DetailCard>
+        {questionData ? (
+          <DetailCardContent>
+            <div>
+              <Label>問題名</Label>
+              {questionData.name}
+            </div>
+            <div>
+              <Label>作成者</Label>
+              {/* {CreatedBy ? CreatedBy : ''} */}
+            </div>
+            <div>
+              <Label>問題形式</Label>
+              {questionData.mode}
+            </div>
+            <div>
+              <Label>作成日</Label>
+              {questionData.created_at}
+            </div>
+            <div>{parse(questionData.card_question.explain)}</div>
+          </DetailCardContent>
+        ) : (
+          ''
+        )}
+      </DetailCard>
+    </div>
+  );
 };
 
 export default QuestionDetail;
