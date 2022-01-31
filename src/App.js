0a4825f42707242159e8fc034e53a949098740d4
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-31 20:00:14 +0900

    feat: Context作成に伴いコード修正

--- src/App.js
+++ src/App.js
@@ -20,45 +20,50 @@ import QuestionDetail from './pages/Question/QuestionDetail';
 //問題作成・編集ページ
 import CreateBlankQuestion from './pages/Question/create/CreateBlankQuestion';
 
+//ContextのProvider
+import {QuestionProvider} from './contexts/QuestionContext';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <Router>
-      <MuiThemeProvider>
-        <div>
-          <Header></Header>
-          <Switch>
-            {/* 一覧ページのルーティング */}
-            <Route exact path='/group'>
-              <GroupList />
-            </Route>
-            <Route exact path='/users'>
-              <UserList />
-            </Route>
-            <Route exact path='/questions'>
-              <QuestionList />
-            </Route>
-            <Route exact path='/teachingMaterials'>
-              <BookList />
-            </Route>
-            <Route exact path='/'>
-              <Home />
-            </Route>
+      <QuestionProvider>
+        <MuiThemeProvider>
+          <div>
+            <Header></Header>
+            <Switch>
+              {/* 一覧ページのルーティング */}
+              <Route exact path='/group'>
+                <GroupList />
+              </Route>
+              <Route exact path='/users'>
+                <UserList />
+              </Route>
+              <Route exact path='/questions'>
+                <QuestionList />
+              </Route>
+              <Route exact path='/teachingMaterials'>
+                <BookList />
+              </Route>
+              <Route exact path='/'>
+                <Home />
+              </Route>
 
-            {/* 詳細ページのルーティング */}
+              {/* 詳細ページのルーティング */}
 
-            <Route render={() => <GroupDetail />} path='/group/detail/:id(\d+)' />
+              <Route render={() => <GroupDetail />} path='/group/detail/:id(\d+)' />
 
-            <Route render={() => <BookDetail />} path='/book/detail/:id(\d+)' />
+              <Route render={() => <BookDetail />} path='/book/detail/:id(\d+)' />
 
-            <Route render={() => <QuestionDetail />} path='/question/detail/:id(\d+)' />
+              <Route render={() => <QuestionDetail />} path='/question/detail/:id(\d+)' />
 
-            {/*  問題作成・編集ページのルーティング */}
-            <Route render={() => <CreateBlankQuestion />} path='/question/create/blank' />
-          </Switch>
-        </div>
-      </MuiThemeProvider>
+              {/*  問題作成・編集ページのルーティング */}
+              <Route render={() => <CreateBlankQuestion />} path='/question/create/blank' />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </QuestionProvider>
     </Router>
   );
 };
--- src/hooks/useQuestion.js
+++ src/hooks/useQuestion.js
@@ -1,8 +1,9 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useContext} from 'react';
 import {getQuestions} from '../components/API/QuestionAPIs';
+import {QuestionContext} from '../contexts/QuestionContext';
 
 export const useQuestion = () => {
-  const [questions, setQuestions] = useState();
+  const {questions, setQuestions} = useContext(QuestionContext);
 
   useEffect(() => {
     getQuestions().then((json) => setQuestions(json));
