1bc5396c5bcbb9cb5ddb3bb98c532ce43eb58785
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-01-31 21:49:00 +0900

    feat: Loading画面の表示作成

--- src/components/Utilities/Loading.js
+++ src/components/Utilities/Loading.js
@@ -1,13 +1,46 @@
 import styled from 'styled-components';
+import {Color} from '../../constants/Color';
+import {BallTriangle} from 'react-loader-spinner';
 
 const StyledLoadingWindow = styled.div`
-  background-color: #fff;
-  width: 100vw;
-  height: 100vh;
   z-index: 5;
-  color: black;
 `;
 
+const LoadingWindowOverlay = styled.div`
+  background-color: ${Color['blackGrey']};
+  width: 100vw;
+  height: 100vh;
+  opacity: 50%;
+`;
+
+const LoadingContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: grid;
+  grid-row-gap: 15px;
+  justify-items: center;
+`;
+
+const LoadingText = styled.div`
+  color: white;
+  font-size: 30px;
+  letter-spacing: 2px;
+`;
+
+const StyledLoader = styled.div``;
+
 export const LoadingWindow = () => {
-  return <StyledLoadingWindow>ロード中...</StyledLoadingWindow>;
+  return (
+    <StyledLoadingWindow>
+      <LoadingWindowOverlay></LoadingWindowOverlay>
+      <LoadingContent>
+        <StyledLoader>
+          <BallTriangle ariaLabel='LoadingIndicator' width={150} height={150} color='blue' secondaryColor='white' />
+        </StyledLoader>
+        <LoadingText>Loading Now...</LoadingText>
+      </LoadingContent>
+    </StyledLoadingWindow>
+  );
 };
