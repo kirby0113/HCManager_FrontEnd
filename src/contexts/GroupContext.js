39fbf3f4e294900f95f14fa32484576e1ecb6df1
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-02-02 22:12:32 +0900

    fix: selectGroupの初期値修正

--- src/contexts/GroupContext.js
+++ src/contexts/GroupContext.js
@@ -4,7 +4,7 @@ export const GroupContext = createContext();
 
 export const GroupProvider = (props) => {
   const [groups, setGroups] = useState([]);
-  const [selectGroup, setSelectGroup] = useState();
+  const [selectGroup, setSelectGroup] = useState({name: '', summary: '', access_key: '', user_id: ''});
   return (
     <GroupContext.Provider value={{groups: groups, setGroups, selectGroup: selectGroup, setSelectGroup}}>
       {props.children}
