d77877a76de00797838bdec3c909129d1e869c96
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-02-23 19:21:07 +0900

    style: DetailCardスタイル微修正

--- src/components/Cards/DetailCard.js
+++ src/components/Cards/DetailCard.js
@@ -21,7 +21,7 @@ const StyledDetailCardContent = styled.div`
   padding-bottom: 20px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-row-gap: 20px;
+  grid-row-gap: 30px;
 `;
 
 const StyledDetailCardSummary = styled.div`
