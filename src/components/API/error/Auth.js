17b9f24d18c2dd009f4138fd67058484985f6334
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-04 11:47:40 +0900

    feat: Authのエラー処理のコード追加

--- src/components/API/error/Auth.js
+++ src/components/API/error/Auth.js
@@ -1,7 +1,16 @@
-export const loginErrorCatch = (status = -1) => {
+export const loginErrorCatch = (status) => {
   switch (status) {
     case -1: {
       return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 404: {
+      return {status: 'fail', content: 'ログインに失敗しました。入力されたユーザー情報が見つかりません。'};
+    }
+    case 401: {
+      return {status: 'fail', content: 'ログインに失敗しました。パスワードが違います。'};
+    }
+    default: {
+      return {status: 'fail', content: 'ログインに失敗しました。定義されていないエラーです。'};
     }
   }
 };
