3cc7e9cf9432966c9360b93fcbb4583f6ac027df
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-04 13:46:43 +0900

    fix: エラーメッセージ修正

--- src/components/API/error/Auth.js
+++ src/components/API/error/Auth.js
@@ -20,15 +20,23 @@ export const registerErrorCatch = (status) => {
     case -1: {
       return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
     }
+
+    case 400: {
+      return {
+        status: 'fail',
+        content: '新規登録に失敗しました。すでにメールアドレスが登録されている可能性があります。',
+      };
+    }
+
     case 404: {
       return {
         status: 'fail',
         content:
-          'ログインに失敗しました。APIが見つからないか、定義されていない権限情報を入力している可能性があります。',
+          '新規登録に失敗しました。APIが見つからないか、定義されていない権限情報を入力している可能性があります。',
       };
     }
     default: {
-      return {status: 'fail', content: 'ログインに失敗しました。定義されていないエラーです。'};
+      return {status: 'fail', content: '新規登録に失敗しました。定義されていないエラーです。'};
     }
   }
 };
