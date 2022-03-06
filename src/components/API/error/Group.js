8ebf4bdbb6283a256773b36f1cb3a7238f507d03
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-06 20:50:02 +0900

    fix: エラーコード修正

--- src/components/API/error/Group.js
+++ src/components/API/error/Group.js
@@ -4,13 +4,13 @@ export const getGroupsErrorCatch = (status) => {
       return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
     }
     case 400: {
-      return {status: 'fail', content: 'Group作成に失敗しました。API側でエラーが発生しました。'};
+      return {status: 'fail', content: 'クラス作成に失敗しました。API側でエラーが発生しました。'};
     }
     case 404: {
-      return {status: 'fail', content: 'Group一覧取得に失敗しました。APIサーバーのエンドポイントが見つかりません。'};
+      return {status: 'fail', content: 'クラス一覧取得に失敗しました。APIサーバーのエンドポイントが見つかりません。'};
     }
     default: {
-      return {status: 'fail', content: 'Group一覧取得に失敗しました。定義されていないエラーです。'};
+      return {status: 'fail', content: 'クラス一覧取得に失敗しました。定義されていないエラーです。'};
     }
   }
 };
@@ -21,13 +21,13 @@ export const getGroupErrorCatch = (status) => {
       return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
     }
     case 400: {
-      return {status: 'fail', content: 'Group作成に失敗しました。API側でエラーが発生しました。'};
+      return {status: 'fail', content: 'クラス取得に失敗しました。API側でエラーが発生しました。'};
     }
     case 404: {
-      return {status: 'fail', content: 'Group取得に失敗しました。該当グループが見つかりません。'};
+      return {status: 'fail', content: 'クラス取得に失敗しました。該当グループが見つかりません。'};
     }
     default: {
-      return {status: 'fail', content: 'Group取得に失敗しました。定義されていないエラーです。'};
+      return {status: 'fail', content: 'クラス取得に失敗しました。定義されていないエラーです。'};
     }
   }
 };
