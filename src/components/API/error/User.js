f7ca9a6c245c74f159eb5c45d7f8e273fbac9dc8
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-07 23:14:48 +0900

    feat: ユーザー作成のエラー処理作成

--- src/components/API/error/User.js
+++ src/components/API/error/User.js
@@ -31,3 +31,20 @@ export const getUsersErrorCatch = (status) => {
     }
   }
 };
+
+export const createUserErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: 'ユーザー作成に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: 'ユーザー作成に失敗しました。APIが見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: 'ユーザー作成に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
