72b34f20689da537814324fbbfd19b858c472813
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-08 15:06:06 +0900

    feat: Book作成時のエラー処理作成

--- src/components/API/error/Book.js
+++ src/components/API/error/Book.js
@@ -7,7 +7,7 @@ export const getBookErrorCatch = (status) => {
       return {status: 'fail', content: '教材取得に失敗しました。API側でエラーが発生しました。'};
     }
     case 404: {
-      return {status: 'fail', content: '教材取得に失敗しました。該当ユーザーが見つかりません。'};
+      return {status: 'fail', content: '教材取得に失敗しました。該当教材が見つかりません。'};
     }
     default: {
       return {status: 'fail', content: '教材取得に失敗しました。定義されていないエラーです。'};
@@ -24,10 +24,27 @@ export const getBooksErrorCatch = (status) => {
       return {status: 'fail', content: '教材一覧取得に失敗しました。API側でエラーが発生しました。'};
     }
     case 404: {
-      return {status: 'fail', content: '教材一覧取得に失敗しました。該当ユーザーが見つかりません。'};
+      return {status: 'fail', content: '教材一覧取得に失敗しました。教材が見つかりません。'};
     }
     default: {
       return {status: 'fail', content: '教材一覧取得に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
+
+export const createBookErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: '教材作成に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: '教材作成に失敗しました。APIが見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: '教材作成に失敗しました。定義されていないエラーです。'};
     }
   }
 };
