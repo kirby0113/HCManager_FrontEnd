c839555e27748407596cb09d5b9ac21808b2dd86
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-08 17:44:49 +0900

    feat: 教材内問題を取得する際のエラー処理作成

--- src/components/API/error/Book.js
+++ src/components/API/error/Book.js
@@ -82,3 +82,20 @@ export const updateBookErrorCatch = (status) => {
     }
   }
 };
+
+export const getRecodeErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: '教材更新に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'not-found', content: []};
+    }
+    default: {
+      return {status: 'fail', content: '教材更新に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
