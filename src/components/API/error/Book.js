1c612a8a69c5467c2c0936a83e9b1c05ae682884
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-08 13:39:16 +0900

    feat: Bookのエラー処理作成

---
+++ src/components/API/error/Book.js
@@ -0,0 +1,16 @@
+export const getBookErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: '教材取得に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: '教材取得に失敗しました。該当ユーザーが見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: '教材取得に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
