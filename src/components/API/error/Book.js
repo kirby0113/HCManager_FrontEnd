7b8d8891a6a0355299b1483151ed8bcd1f96ef63
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-08 15:32:48 +0900

    feat: 教材削除のエラー処理作成

--- src/components/API/error/Book.js
+++ src/components/API/error/Book.js
@@ -48,3 +48,20 @@ export const createBookErrorCatch = (status) => {
     }
   }
 };
+
+export const deleteBookErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: '教材削除に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: '教材削除に失敗しました。該当教材が見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: '教材削除に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
