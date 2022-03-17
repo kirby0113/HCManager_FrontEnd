3fa5a7df93bb7e9281b420be5ea087a7fef196ba
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-17 22:14:01 +0900

    feat: グループ内教材取得時のエラー処理作成

--- src/components/API/error/Group.js
+++ src/components/API/error/Group.js
@@ -48,3 +48,20 @@ export const createGroupErrorCatch = (status) => {
     }
   }
 };
+
+export const getGroupInBooksErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: 'クラス内教材一覧取得に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: 'クラス内教材一覧取得に失敗しました。エンドポイントが見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: 'クラス内教材一覧取得に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
