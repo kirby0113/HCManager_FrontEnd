c81de2ff28a33d1ce42cf0af3066dffb2236191e
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-18 20:26:48 +0900

    feat: クラス編集時のエラーメッセージ作成

--- src/components/API/error/Group.js
+++ src/components/API/error/Group.js
@@ -49,6 +49,23 @@ export const createGroupErrorCatch = (status) => {
   }
 };
 
+export const updateGroupErrorCatch = (status) => {
+  switch (status) {
+    case -1: {
+      return {status: 'fail', content: 'ネットワークエラーです。VPNの接続状況などを確認してください。'};
+    }
+    case 400: {
+      return {status: 'fail', content: 'クラス更新に失敗しました。API側でエラーが発生しました。'};
+    }
+    case 404: {
+      return {status: 'fail', content: 'クラス更新に失敗しました。該当のクラスが見つかりません。'};
+    }
+    default: {
+      return {status: 'fail', content: 'クラス更新に失敗しました。定義されていないエラーです。'};
+    }
+  }
+};
+
 export const getCollectionsErrorCatch = (status) => {
   switch (status) {
     case -1: {
